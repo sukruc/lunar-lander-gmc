9377fe13c81b3f3e82802d34c0a17744b4517785
ssezer <sukrucsezer@outlook.com>
2020-12-25 12:23:49 -0800

    update weights

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Run `land.py` to land on Moon!
 
 Run `sga_train.py` to train a new agent.
 
+See [notes](#Notes-for-Windows-Users) if having problems with PyBox2D.
+
 ## Agents
 ---
 
@@ -21,6 +23,12 @@ A GMC [[1]] agent implementation for solving Lunar Landing task.
 Gradient Monte Carlo algorithm works on the basis of SGD, sampling states and rewards from an environment using provided policy and updating a differentiable value-approximation function at the end of sequence.
 
 Weight update is performed separately for each state-action tuple, rather than accumulating the gradient over all sequence.
+
+## Notes for Windows Users
+
+If having problems installing PyBox2D using pip, download the wheel from [here](https://www.lfd.uci.edu/~gohlke/pythonlibs/#pybox2d)
+and install the package using `pip install [wheel filename]`.
+
 
 ## References
 ---
--- create_from_trained.py
+++ create_from_trained.py
@@ -5,41 +5,48 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-print('Creating environment...')
-env = LunarLander()
-print('Done.')
+TRAINED_WEIGHTS = 'trained_agent_weights.npy'
 
 
-print('Loading agent...')
-agent = GMCAgent()
-agent.set_weights(np.load('trained_agent_weights.npy'))
-
-print('Testing agent...')
-trained_scores = [agent.land(env, verbose=False, render=False) for _ in range(100)]
-print('Done.')
+def main():
+    print('Creating environment...')
+    env = LunarLander()
+    print('Done.')
 
 
-print('Creating figures...')
-plt.figure()
-plt.hist([s[0] for s in trained_scores], bins=20)
-plt.ylabel('Count')
-plt.grid()
-plt.text(100, 20, f'Mean total reward: {np.mean([s[0] for s in trained_scores])}')
-_ = plt.xlabel('Total Rewards')
-plt.savefig('trained_figures/rewards_test.png')
+    print('Loading agent...')
+    agent = GMCAgent()
+    agent.set_weights(np.load(TRAINED_WEIGHTS))
 
-# plt.figure()
-# pd.Series(agent.history['rewards']).rolling(50, min_periods=1).mean().plot()
-# plt.xlabel('Episodes')
-# plt.ylabel('Total Reward')
-# plt.savefig('trained_figures/rewards_train.png')
+    print('Testing agent...')
+    trained_scores = [agent.land(env, verbose=False, render=False) for _ in range(100)]
+    print('Done.')
 
-plt.figure()
-pd.Series([s[1] for s in trained_scores]).value_counts().plot(kind='barh')
-plt.ylabel('Final Reward')
-plt.grid()
-_ = plt.xlabel('Count')
-plt.savefig('trained_figures/success_count.png')
 
-plt.close('all')
-print('Done.')
+    print('Creating figures...')
+    plt.figure()
+    plt.hist([s[0] for s in trained_scores], bins=20)
+    plt.ylabel('Count')
+    plt.grid()
+    plt.text(100, 20, f'Mean total reward: {np.mean([s[0] for s in trained_scores])}')
+    _ = plt.xlabel('Total Rewards')
+    plt.savefig('trained_figures/rewards_test.png')
+
+    # plt.figure()
+    # pd.Series(agent.history['rewards']).rolling(50, min_periods=1).mean().plot()
+    # plt.xlabel('Episodes')
+    # plt.ylabel('Total Reward')
+    # plt.savefig('trained_figures/rewards_train.png')
+
+    plt.figure()
+    pd.Series([s[1] for s in trained_scores]).value_counts().plot(kind='barh')
+    plt.ylabel('Final Reward')
+    plt.grid()
+    _ = plt.xlabel('Count')
+    plt.savefig('trained_figures/success_count.png')
+
+    plt.close('all')
+    print('Done.')
+
+if __name__ == '__main__':
+    main()
